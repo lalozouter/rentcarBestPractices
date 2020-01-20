63bd3290d310ab3a115f6d3f7417251a4212e29a
lalozouter <lalozouter@gmail.com>
2020-01-20 14:27:03 -0600

    commit Vehicle

---
+++ rentcloud-commons/src/main/java/com/latzche/rentcloud/commons/model/Vehicle.java
@@ -0,0 +1,53 @@
+package com.latzche.rentcloud.commons.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="vehicle")
+public class Vehicle {
+
+	@Id
+	@GeneratedValue
+	Integer id;
+	String make;
+	String model;
+	String type;
+	Integer year;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+	
+	
+}
